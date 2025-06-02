deb745f0331c5d8c5bbd3815abe034500a701a3b
punit1703 <pr17032006@gmail.com>
2025-06-02 13:49:31 +0530

    Add auto superuser creation in apps.py

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'users',
+    'users.apps.UsersConfig'
     'tailwind',
     'theme',
     'django_browser_reload',
--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        if os.environ.get('ENVIRONMENT') == 'production':
+            User = get_user_model()
+            if not User.objects.filter(is_superuser=True).exists():
+                User.objects.create_superuser(
+                    username=os.environ.get('DJANGO_SUPERUSER_USERNAME', 'punit'),
+                    email=os.environ.get('DJANGO_SUPERUSER_EMAIL', 'pr17032006@gmail.com'),
+                    password=os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpass123'),
+                )

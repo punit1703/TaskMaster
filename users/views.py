9586eb3391ae7b6b290d22b1bc68a118d5228ab9
punit1703 <pr17032006@gmail.com>
2025-06-02 12:29:38 +0530

    fixed the login issue

--- users/views.py
+++ users/views.py
@@ -3,25 +3,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def userProfile(request):
-    tasks_count = Task.objects.filter(user = request.user).count()
-    completed_count = Task.objects.filter(user = request.user, is_completed=True).count()
-    not_completed = tasks_count  - completed_count
+    tasks_count = Task.objects.filter(user=request.user).count()
+    completed_count = Task.objects.filter(user=request.user, is_completed=True).count()
+    not_completed = tasks_count - completed_count
+
     context = {
-        'tasks_count': tasks_count, 
-        'completed_count':completed_count,
+        'tasks_count': tasks_count,
+        'completed_count': completed_count,
         'not_completed': not_completed,
     }
-    return render(request,'registration/profile.html', context)
+    return render(request, 'registration/profile.html', context)
+
 
 def loginPage(request):
     error = False
 
     if request.user.is_authenticated:
         return redirect('home')
-
 
     if request.method == 'POST':
         username = request.POST.get('username').lower()
@@ -37,6 +40,7 @@ def loginPage(request):
 
     context = {'page': 'login', 'error': error}
     return render(request, 'registration/login.html', context)
+
 
 def logoutUser(request):
     logout(request)
@@ -56,6 +60,6 @@ def registerPage(request):
         else:
             user = User.objects.create_user(username=username, password=password)
             login(request, user)
-            return redirect('home')
+            return redirect('home') 
 
     return render(request, 'registration/register.html')

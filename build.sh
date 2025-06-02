95e7780206620a94c790b463ce595b6f03edac4c
punit1703 <pr17032006@gmail.com>
2025-06-02 14:21:22 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Install Tailwind dependencies
-npm install
-python manage.py tailwind install
-python manage.py tailwind build
-
-# DB and static setup
-python manage.py migrate
-python manage.py collectstatic --noinput
-
-
-
-#!/usr/bin/env bash
-
 pip install -r requirements.txt
 
 npm install

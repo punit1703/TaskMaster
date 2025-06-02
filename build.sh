f1086cf2293db7352ed166a95d243b8b05b2a97f
punit1703 <pr17032006@gmail.com>
2025-06-02 14:17:48 +0530

    .env

--- build.sh
+++ build.sh
@@ -11,3 +11,16 @@ python manage.py tailwind build
 # DB and static setup
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+
+
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+npm install
+python manage.py tailwind install
+python manage.py tailwind build
+
+python manage.py migrate
+python manage.py collectstatic --noinput

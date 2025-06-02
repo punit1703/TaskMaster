5f2067ee20fb19c56375c72fdb96e3aeaf22a07f
punit1703 <pr17032006@gmail.com>
2025-06-02 12:08:06 +0530

    fixed the tailwind issue

---
+++ theme/tailwind.config.js
@@ -0,0 +1,12 @@
+// theme/tailwind.config.js
+module.exports = {
+  content: [
+    '../../templates/**/*.html',
+    '../../**/templates/**/*.html',
+    '../../**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

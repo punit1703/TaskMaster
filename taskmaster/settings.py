cd12ac4ab76dd253d6a460f4ca0a7c3e02554b54
Punit Patel <105913404+punit1703@users.noreply.github.com>
2025-07-02 22:58:28 +0530

    Update settings.py

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -19,7 +19,11 @@ import os
 load_dotenv()
 
 DATABASES = {
-    'default': os.getenv("DATABASE_URL")
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 env = Env()
